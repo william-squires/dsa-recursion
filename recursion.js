49a2d8308eb06c64540ebd072fd35e3f48581070
William J Squires <william.squiresii@gmail.com>
2023-05-30 14:11:38 -0500

    did stuff

--- recursion.js
+++ recursion.js
@@ -1,36 +1,95 @@
-/** product: calculate the product of an array of numbers. */
+/** product: calculate the product of an array of numbers.
+ * [2,3,4] -> 24
+*/
 
 function product(nums) {
-
+  if (nums.length === 0) {
+    return 1;
+  } else {
+    return nums[0] * product(nums.slice(1));
+  }
 }
 
-/** longest: return the length of the longest word in an array of words. */
-
+/** longest: return the length of the longest word in an array of words.
+ * ["hello", "hi", "hola"] -> 5
+*/
+//[] = 0
 function longest(words) {
+  if (words.length === 0) {
+    return 0;
+  } else {
+    longestWord = longest(words.slice(1));
+    if (words[0].length > longestWord) {
+      return words[0].length;
+    } else {
+      return longestWord;
+    }
+  }
 
 }
 
-/** everyOther: return a string with every other letter. */
-
+/** everyOther: return a string with every other letter.
+ * "hello" -> "hlo"
+*/
+// "" = ""
+// "o" = "o"
+// "llo" = "lo"
+// "hello" = "hlo"
 function everyOther(str) {
+  if (str.length === 0) {
+    return "";
+  } else {
+    return str[0] + everyOther(str.slice(2));
+  }
 
 }
 
 /** find: return boolean depending on if val exists in array or not. */
 
 function find(arr, val) {
-
+  if (arr.length === 0) {
+    return false;
+  } else {
+    if (arr[0] === val) {
+      return true;
+    } else {
+      return find(arr.slice(1), val);
+    }
+  }
 }
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
-
+// "" = true
+// "a" =true
+// "ab" = false
+// "aba" = true
+// "tacocat" = true
+// "acoca"
 function isPalindrome(str) {
-
+  if (str.length <= 1) {
+    return true;
+  } else {
+    if (str[0] === str[str.length - 1]) {
+      return isPalindrome(str.slice(1, str.length-1))
+    } else {
+      return false;
+    }
+  }
 }
 
-/** revString: return a copy of a string, but in reverse. */
+/** revString: return a copy of a string, but in reverse.
+ * "k"
+ * "ck"
+ * "uck"
+ * "duck"
+*/
 
 function revString(str) {
+  if (str === "") {
+    return ""
+  } else {
+    return revString(str.slice(1)) + str[0];
+  }
 
 }
 
